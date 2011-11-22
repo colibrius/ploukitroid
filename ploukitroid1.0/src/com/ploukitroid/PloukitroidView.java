9b758590a3c14a10b46780f2ede262d525819849
Maxime VdG <mvdeghin@gmail.com>
2011-11-22 17:34:05 +0100

    plak

--- ploukitroid1.0/src/com/ploukitroid/PloukitroidView.java
+++ ploukitroid1.0/src/com/ploukitroid/PloukitroidView.java
@@ -1,13 +1,18 @@
 package com.ploukitroid;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 
 public class PloukitroidView extends View {
-	
+	Drawable bg;
+	Canvas canvas;
 	public PloukitroidView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+		
+		this.setBackgroundResource(R.drawable.ic_launcher);
 	}
 
 }
