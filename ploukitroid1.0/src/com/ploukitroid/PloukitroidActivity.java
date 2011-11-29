6004e801f7513ff8eebf2fc75188393ab15f71cd
Maxime VdG <mvdeghin@gmail.com>
2011-11-29 10:46:43 +0100

    Commit again !

--- ploukitroid1.0/src/com/ploukitroid/PloukitroidActivity.java
+++ ploukitroid1.0/src/com/ploukitroid/PloukitroidActivity.java
@@ -2,16 +2,83 @@ package com.ploukitroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnCreateContextMenuListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class PloukitroidActivity extends Activity {
-    /** Called when the activity is first created. */
+public class PloukitroidActivity extends Activity implements
+		View.OnTouchListener, OnCreateContextMenuListener {
+	/** Called when the activity is first created. */
 	PloukitroidView pView;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        pView = new PloukitroidView(this);
-        setContentView(pView);
-    }
+	Button bStart;
+	Button bStop;
+	Ship ship;
+	boolean bMenu;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		this.setContentView(R.layout.main);
+
+		bStart = (Button) findViewById(R.id.bStart);
+		bStart.setText("Start");
+		bStart.setOnTouchListener(this);
+		bStop = (Button) findViewById(R.id.bStop);
+		bStop.setText("Stop");
+		bStop.setOnTouchListener(this);
+		bMenu = true;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		switch (v.getId()) {
+		// Start application :
+		case R.id.bStart:
+			ship = new Ship(pView, this);
+			pView = new PloukitroidView(getApplicationContext(), ship);
+
+			this.setContentView(pView);
+			pView.setOnTouchListener(ship);
+			break;
+
+		// Press stop button
+		case R.id.bStop:
+			this.finish();
+			break;
+		}
+		return false;
+
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+			this.setContentView(R.layout.main);
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
 }
--- ploukitroid1.0/src/com/ploukitroid/Ship.java
+++ ploukitroid1.0/src/com/ploukitroid/Ship.java
@@ -1,7 +1,83 @@
 package com.ploukitroid;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.graphics.drawable.shapes.RectShape;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
 
-public class Ship extends RectShape{
+public class Ship extends RectShape implements View.OnTouchListener {
+	private int path;
+	private Rect rectangle;
+	private int posX, posY, width, heigth, oldX, oldY, screenWidth,
+			screenHeigth;
+	private boolean isTouching;
+	private View pView;
+
+	public Ship(View pView, Context context) {
+		path = R.drawable.ic_launcher;
+		posX = 100;
+		posY = 100;
+		oldX = posX;
+		oldY = posY;
+		width = 100;
+		heigth = 100;
+		rectangle = new Rect(posX - width / 2, posY - heigth / 2, posX + width
+				/ 2, posY + heigth / 2);
+		isTouching = false;
+		this.pView = pView;
+
+		Display display = ((WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+		screenHeigth = display.getHeight();
+		screenWidth = display.getWidth();
+	}
+
+	public int getPath() {
+		return path;
+	}
+
+	public Rect getRect() {
+		return rectangle;
+	}
+
+	public void setPosition(int x, int y, MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN)
+			if (x >= rectangle.left && x <= rectangle.right)
+				if (y >= rectangle.top && y <= rectangle.bottom) {
+					isTouching = true;
+					oldX = x;
+					oldY = y;
+				} else if (event.getAction() == (MotionEvent.ACTION_UP))
+					isTouching = false;
+
+		// To avoid "teleport" when multi-touching
+		if (Math.abs(x - oldX) > 199 || Math.abs(y - oldY) > 199)
+			isTouching = false;
+		if (isTouching) {
+			if (x < width / 2)
+				x = width / 2;
+			else if (x / screenWidth > 0.25)
+				x = screenWidth / 4;
+			if (y < width / 2)
+				y = width / 2;
+			rectangle.set(x - width / 2, y - heigth / 2, x + width / 2, y
+					+ heigth / 2);
+		}
+		oldX = x;
+		oldY = y;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		int x = (int) event.getX();
+		int y = (int) event.getY();
+
+		this.setPosition(x, y, event);
+		v.invalidate();
+		return false;
+	}
 
 }
